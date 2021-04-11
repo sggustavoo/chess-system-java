385a69b0babc04cf1296620bdb77ca07c477421a
Gustavo Silva <g.gustavosilva@hotmail.com>
2021-04-11 13:54:09 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
-public class Program {
-	public static void main(String[] args) {
-	
-		System.out.println("Hello World!");
+import boardgame.Position;
 
+public class Program {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello world!");
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
 }
